56e0f2073df698524eb48439dbb6637324ca121f
osamu <osamu@quwans.jp>
2019-03-20 11:10:18 +0900

    微編集

--- hello.js
+++ hello.js
@@ -1,10 +1,19 @@
 //const debug = require('debug');
-const http = require('http');
+const express = require('express');
+const app = express();
+//サーバデブロイ時には、「https」にすると、ワーニングが出なくなる。ローカル接続では、https証明の発行ができいないので、だめ。
+const http = require("http").Server(app);
+
+const path = require('path');
+const favicon = require('serve-favicon');
+const bodyParser = require('body-parser');
+const fs = require('fs');
+
 const PORT = 8080;
 
-http.createServer((request, response) => {
-response.writeHead(200, {'Content-Type': 'text/plain'});
-response.end('Hello World!\n');
-}).listen(PORT);
+//http.createServer((request, response) => {
+//response.writeHead(200, {'Content-Type': 'text/plain'});
+//response.end('Hello World!\n');
+//}).listen(PORT);
 
-console.log(`Server running at http://localhost:${PORT}/`);
+console.log('Server running at  http:133.167.47.45:80');
