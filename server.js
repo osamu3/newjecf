07a6fc47eb29af429a8660cb3b829a5394d0f6af
osamu <osamu@quwans.jp>
2019-03-20 19:14:00 +0900

    ここから出発

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const port = 8080;
-
+const favicon = require('serve-favicon');
 
 // app.use/routes/etc...
 //
