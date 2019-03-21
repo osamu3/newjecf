25b9b51f67d104a1356b70e2945b6c43832513c8
osamu <osamu@quwans.jp>
2019-03-21 09:19:21 +0900

    routes/index.js And views/index.ejs added

---
+++ routes/index.js
@@ -0,0 +1,16 @@
+﻿'use strict';
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function (req, res) {
+    res.render('index', { title: 'GBird',content:'hogehoge' });
+});
+
+module.exports = router;
+//D
+//↑このモジュールは、server.jsから、↓として使用されている。
+//const routes = require('./routes/index');
+//app.use('/', routes);//ルートへのアクセスで、↑で定義したroutesを呼び出す。
+//server.js内では、「route」ではなく「routes」と複数形になっていることに注意。
+//
--- server.js
+++ server.js
@@ -6,20 +6,21 @@ const port = 8080;
 const favicon = require('serve-favicon');
 
 // app.use/routes/etc...
-const routes = require('./routes/index');/
-
-/ view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+const routes = require('./routes/index');
 
 const server = app.listen(port, () => console.log('Example app listening on port 8080!'));
+const io     = require('socket.io').listen(server);
 
-var io     = require('socket.io').listen(server);
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
 
 // io.sockets.on('connection', function (socket) {
 //   ...
 //   });
-
-
-app.get('/', (req, res) => res.send('Hello World!'));
+app.use('/', routes);//ルートへのアクセスで、↑で定義したroutesを呼び出す。
+//routesは、./routes/index.jsを呼び出す。
+//routesは、./routes/index.js内で、「route」が定義されている。
+//app.get('/', (req, res) => res.send('Hello World!'));
 
